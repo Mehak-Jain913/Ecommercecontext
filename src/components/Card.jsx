01aa47812f4fd083df67cac8c1e5c21e6c6e2d16
Mehak-Jain913 <mehakj536@gmail.com>
2026-05-01 00:07:48 +0530

    Fix case-sensitive CSS import

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react"
-import "./Card.css"
+import "./card.css"
 import { Link } from "react-router-dom"
 import { ProductContext } from "../context/ProductContext"
 
